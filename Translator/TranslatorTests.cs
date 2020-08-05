02812425a5d18a575bb046bcb3d144290650467b
Dan Schlossberg <daniel.schlossberg@coxautoinc.com>
2020-08-05 12:11:11 -0700

    word starts with vowel test

--- Translator/TranslatorTests.cs
+++ Translator/TranslatorTests.cs
@@ -1,38 +1,60 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Translator
 {
     public class TranslatorTests
     {
+        private readonly ITestOutputHelper _console;
+        private readonly Translator        _translator;
+
+        public TranslatorTests(ITestOutputHelper console)
+        {
+            _console     = console;
+            _translator = new Translator();
+        }
+
         [Fact]
         public void SampleTest()
         {
             // arrange
             var input          = "Birds of feather flock together";
             var expectedOutput = "irdsbay ofay eatherfay lockfay ogethertay";
-            var translator     = new Translator();
 
             // act
-            var output         = translator.Translate(input);
+            var output         = _translator.Translate(input);
 
             // assert
             Assert.Equal(expectedOutput, output);
         }
 
         [Fact]
-        public void NullReturnsNull()
+        public void Null_Returns_Null()
         {
             // arrange
             string input          = null;
             string expectedOutput = null;
-            var translator        = new Translator();
 
             // act
-            var output = translator.Translate(input);
+            var output = _translator.Translate(input);
 
             // assert
             Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void WordStartsWithVowel_Appends_Ay()
+        {
+            // arrange
+            var word = "of";
+            var expected = "ofay";
+
+            // act
+            var output = _translator.TranslateWord(word);
+
+            // assert
+            Assert.Equal(expected, output);
         }
     }
 }
