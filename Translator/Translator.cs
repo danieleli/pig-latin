50225841c3ddf723fe92a642dfb9b67df432ad22
Dan Schlossberg <daniel.schlossberg@coxautoinc.com>
2020-08-05 11:47:51 -0700

    initial test

---
+++ Translator/Translator.cs
@@ -0,0 +1,10 @@
+namespace Translator
+{
+    public class Translator
+    {
+        public string? Translate(string input)
+        {
+            return null;
+        }
+    }
+}
---
+++ Translator/TranslatorTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace Translator
+{
+    public class TranslatorTests
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            // arrange
+            var input          = "Birds of feather flock together";
+            var expectedOutput = "irdsbay ofay eatherfay lockfay ogethertay";
+            var translator     = new Translator();
+
+            // act
+            var output         = translator.Translate(input);
+
+            // assert
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
